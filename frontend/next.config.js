9299b22f21aa70789395b1972f13b9f4aaad84dd
Harshavardhan . G <141905205+G-Harshavardhan@users.noreply.github.com>
2026-04-16 15:31:05 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
         return [
             {
                 source: '/api/:path*',
-                destination: 'https://quiz-app-production-72cb.up.railway.app/api/:path*'
+                destination: 'https://quiz-app-production-4c0a.up.railway.app/api/:path*'
             }
         ];
     }
